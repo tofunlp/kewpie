7c13b13677411b73948786a5049c5fe43129d2fa
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2019-07-24 21:26:38 +0900

    Added naive implementation.

--- kewpie/core.py
+++ kewpie/core.py
@@ -1,6 +1,37 @@
+import os
+from typing import List
+import pickle
+
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-class Core:
+def build(docs: List[str], savedir: str) -> None:
+    vectorizer = TfidfVectorizer(docs)
+    vectorizer.fit(docs)
+    with open(os.path.join(savedir, 'vectorizer.pth'), 'wb') as f:
+        pickle.dump(vectorizer, f)
 
-    def say(self):
-        return "Hola mundo!"
+
+class KwPicker(object):
+
+    def __init__(self, vectorizer_path: str):
+        with open(vectorizer_path, 'rb') as f:
+            vectorizer: TfidfVectorizer = pickle.load(f)
+        self.vectorizer = vectorizer
+        self.vocab = vectorizer.get_feature_names()
+        self.tokenizer = vectorizer.build_tokenizer()
+
+    def get_keywords(self, sentence: str) -> (str, int):
+        vocab = np.array(self.vocab)
+
+        docs = [sentence]
+        x = self.vectorizer.transform(docs).todense().reshape(-1)
+
+        vocab_idx = np.argmax(x, axis=1).tolist()
+        keyword = vocab[tuple(vocab_idx)].tolist()[0]
+
+        sent_idx = sentence.find(keyword)
+        span = (sent_idx, sent_idx + len(keyword))
+
+        return span, keyword
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,16 +1,10 @@
 from unittest import TestCase
 
-from kewpie.core import Core
 
-
-class CoreCase(TestCase):
+class KwPickerCase(TestCase):
 
     def setUp(self):
-        self.core = Core()
         pass
 
     def tearDown(self):
         pass
-
-    def test_core(self):
-        self.assertEqual(self.core.say(), "Hola mundo!")
